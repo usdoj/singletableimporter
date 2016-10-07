c54d148abebe84cc4fbe837a8c069103a40ecd61
Brock Fanning <brockfanning@gmail.com>
2016-10-07 13:57:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# SingleTableImporter
+
+This class is intended as a simple importer for moving source data into a single MySQL table. It provides a few automations, such as text alterations, and date alterations, which is why it might be better than simply importing the file with phpMyAdmin or something similar. It supports currently supports CSV files and single-worksheet Excel spreadsheets, and assumes that the first row of data corresponds exactly to the column names in the MySQL database.
+
+## Dependencies
+
+* PHP 5.3.2 or higher
+* MySQL 5.5 or higher
+
+## Dev dependencies
+
+* Composer
+
+## Installation
+
+Use composer to bring this into your PHP project. The composer.json should look like this:
+
+```
+{
+    "require": {
+        "usdoj/singletableimporter": "dev-master"
+    },
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/usdoj/singletableimporter.git"
+        }
+    ]
+}
+```
+
+After creating a composer.json similar to the above, do a "composer install".
+
+## Usage
+
+To use the library you need to include the autoloader, and then instantiate the object referencing your configuration and source data file. The configuration must be an object of the \Noodlehaus\Config class. Then you simply call the run() method. For example:
+
+```
+require_once '/path/to/my/libraries/csvtomysql/vendor/autoload.php';
+$config = new \Noodlehaus\Config('/path/to/my/configFile.yml');
+$sourceFile = '/path/to/my/sourceFile.csv';
+$importer = new \USDOJ\CsvToMysql\Importer($config, $sourceFile);
+$importer->run();
+```
+
+## Configuration
+
+The library depends on configuration in an \Noodlehaus\Config object. Here are examples (in YAML) of the settings that should exist in that config object:
+```
+# Database credentials: this is the only required section.
+database name: myDatabase
+database user: myUser
+database password: myPassword
+database host: localhost
+database table: myTable
+
+# If there are any special characters or phrases that need to be altered when
+# importing the data from the CSV file, indicate those here. For example, to
+# change all occurences of § with &#167; uncomment the lines below.
+#text alterations:
+#    "§": "&#167;"
+```
---
+++ src/Importer.php
@@ -0,0 +1,321 @@
+<?php
+/**
+ * @file
+ * Class for importing data from a source file.
+ */
+
+namespace USDOJ\SingleTableImporter;
+
+/**
+ * Class Importer
+ * @package USDOJ\SingleTableImporter
+ *
+ * A class for importing data from a CSV or Excel file into a MySQL table.
+ */
+class Importer {
+
+    /**
+     * @var \Noodlehaus\Config
+     *   The configuration object.
+     */
+    private $config;
+
+    /**
+     * @var string
+     *   The location of the source file on disk.
+     */
+    private $sourceFile;
+
+    /**
+     * @var \Doctrine\DBAL\Connection
+     *   The database connection.
+     */
+    private $db;
+
+    /**
+     * @var Array dateColumns
+     *   An array of DATETIME columns in the database.
+     */
+    private $dateColumns;
+
+
+    /**
+     * Get the configuration object.
+     *
+     * @return \Noodlehaus\Config
+     */
+    public function getConfig() {
+        return $this->config;
+    }
+
+    /**
+     * Get the location of the source data file.
+     *
+     * @return string
+     */
+    public function getSourceFile() {
+        return $this->sourceFile;
+    }
+
+    /**
+     * Get the database connection.
+     *
+     * @return \Doctrine\DBAL\Connection
+     */
+    public function getDb() {
+        return $this->db;
+    }
+
+    /**
+     * Importer constructor.
+     *
+     * @param \Noodlehaus\Config $config
+     *   The configuration object.
+     * @param string $sourceFile
+     *   The location of the source data file on disk.
+     */
+    public function __construct($config, $sourceFile) {
+
+        if (!is_file($sourceFile)) {
+            die(sprintf('Source data not found at %s', $sourceFile));
+        }
+
+        $this->config = $config;
+        $this->sourceFile = $sourceFile;
+
+        // Start the database connection.
+        $dbConfig = new \Doctrine\DBAL\Configuration();
+        $connectionParams = array(
+            'dbname' => $this->settings('database name'),
+            'user' => $this->settings('database user'),
+            'password' => $this->settings('database password'),
+            'host' => $this->settings('database host'),
+            'port' => 3306,
+            'charset' => 'utf8',
+            'driver' => 'pdo_mysql',
+        );
+        $db = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $dbConfig);
+        $this->db = $db;
+
+        // Catalog all the DATETIME columns for later.
+        // Take note of all the datetime columns so that their facets can be
+        // rendered hierarchically.
+        $this->dateColumns = array();
+        $statement = $this->getDb()->query('DESCRIBE ' . $this->settings('database table'));
+        $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        foreach ($result as $column) {
+            if ('datetime' == $column['Type']) {
+                $this->dateColumns[] = $column['Field'];
+            }
+        }
+    }
+
+    /**
+     * Helper method to get a configuration setting by key.
+     *
+     * @param $key
+     *   The key to get a config setting for.
+     *
+     * @return mixed
+     *   Whatever the config value there is for that key.
+     */
+    public function settings($key) {
+        return $this->getConfig()->get($key);
+    }
+
+    /**
+     * Run the importer.
+     *
+     * @throws \Exception
+     */
+    public function run() {
+
+        $this->delete();
+        $this->insert();
+    }
+
+    /**
+     * Insert the new data into the database table.
+     *
+     * @throws \Exception
+     */
+    private function insert() {
+
+        $inputFileName = $this->getSourceFile();
+
+        try {
+            $rows = $this->dataToArray($inputFileName);
+
+            $table = $this->settings('database table');
+            $numInserted = 0;
+            foreach ($rows as $row) {
+                $anonymousParameters = array();
+                $insert = $this->getDb()->createQueryBuilder()
+                    ->insert($table);
+                foreach ($row as $column => $value) {
+                    if (empty($column)) {
+                        // In some cases the imported data mistakenly has
+                        // empty header columns.
+                        continue;
+                    }
+                    $insert->setValue('`' . $column . '`', '?');
+                    $anonymousParameters[] = $value;
+                }
+                $numInserted += $insert
+                    ->setParameters($anonymousParameters)
+                    ->execute();
+            }
+            print sprintf('Imported %s rows.', $numInserted) . PHP_EOL;
+        } catch (Exception $e) {
+            $pathInfo = pathinfo($inputFileName, PATHINFO_BASENAME);
+            throw new \Exception(sprintf('Error loading file %s: %s', $pathInfo, $e->getMessage()));
+        }
+    }
+
+    /**
+     * Delete all the existing data from the database table.
+     */
+    private function delete() {
+
+        $table = $this->settings('database table');
+        $this->getDb()->createQueryBuilder()
+            ->delete($table)
+            ->execute();
+        print 'Deleted all rows.' . PHP_EOL;
+    }
+
+    /**
+     * Helper method to convert a file into an array.
+     *
+     * @param string $filePath
+     *   The path on disk to the CSV file.
+     *
+     * @return array
+     */
+    private function dataToArray($filePath) {
+
+        $rows = NULL;
+        $header = NULL;
+
+        $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+
+        // Support Excel files.
+        if ('xls' == $extension || 'xlsx' == $extension) {
+            $objReader = PHPExcel_IOFactory::createReader('Excel2007');
+            $objPHPExcel = $objReader->load($filePath);
+            foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+                $rows = $worksheet->toArray();
+                $header = $rows[0];
+                // We only get the first worksheet.
+                break;
+            }
+        }
+        else {
+            // If not an Excel file, assume it is CSV.
+            $rows = new \Keboola\Csv\CsvFile($filePath);
+            $header = $rows->getHeader();
+        }
+
+        $data = array();
+        // Skip the first row since it is the headers.
+        $skip = TRUE;
+        foreach($rows as $row) {
+            if ($skip) {
+                $skip = FALSE;
+                continue;
+            }
+
+            // Do we need to filter any of the text?
+            $filteredRow = $row;
+            $textAlterations = $this->settings('text alterations');
+            if (!empty($textAlterations)) {
+                foreach ($textAlterations as $search => $replace) {
+                    $filteredRow = str_replace($search, $replace, $filteredRow);
+                }
+            }
+            // Do we need to convert any dates?
+            if (!empty($this->dateColumns)) {
+                foreach ($header as $index => $column) {
+                    if (in_array($column, $this->dateColumns)) {
+                        $filteredRow[$index] = $this->convertDate($filteredRow[$index]);
+                    }
+                }
+            }
+            // Fix invalid rows.
+            if (count($filteredRow) < count($header)) {
+                for ($i = 0; $i < count($header) - count($filteredRow); $i++) {
+                    $filteredRow[] = '';
+                }
+            }
+            $data[] = array_combine($header, $filteredRow);
+        }
+        return $data;
+    }
+
+    /**
+     * Convert dates into MySQL DATETIME values.
+     *
+     * This method needs to be very forgiving, because dates can be in a variety
+     * of formats. This supports Excel dates, Unix timestamps, PHP-supported
+     * date strings, and even some date formats that us humans like to use.
+     *
+     * @param int $dateString
+     *   A raw date string to convert.
+     *
+     * @return string
+     */
+    private function convertDate($dateString) {
+
+        // What we're trying to get.
+        $unixTimestamp = NULL;
+
+        if (empty($dateString)) {
+            return $dateString;
+        }
+
+        if (is_numeric($dateString)) {
+            $dateInt = (int) $dateString;
+
+            // The easiest to recognize is Excel's unique date format, which is
+            // the number of days since 1900. For 99% of cases this will be an
+            // integer between -100,000 and 100,000. (If this generalization
+            // ever stops working, we can complicate this library with some new
+            // configuration options.)
+            if ($dateInt > -100000 && $dateInt < 100000) {
+                // Numbers of days between January 1, 1900 and 1970.
+                $daysSince = 25569;
+                // Numbers of second in a day:
+                $secondsInDay = 86400;
+                $unixTimestamp = ($dateInt - $daysSince) * $secondsInDay;
+            }
+            else {
+                // For all other integer dates, we assume that it is already a
+                // Unix timestamp.
+                $unixTimestamp = $dateInt;
+            }
+        }
+        else {
+            // If not numeric, we assume it is a date string. First try to let
+            // PHP figure it out.
+            $unixTimestamp = strtotime($dateString);
+            if (!$unixTimestamp) {
+                // If that didn't work, try for some common human-reable formats
+                // that maybe PHP doesn't support. For example, YYYY-MM is one,
+                // and similarly, YYYY/MM.
+                if (2 == count(explode('/', $dateString))) {
+                    $unixTimestamp = strtotime($dateString . '/01');
+                }
+                elseif (2 == count(explode('-', $dateString))) {
+                    $unixTimestamp = strtotime($dateString . '-01');
+                }
+            }
+        }
+
+        // If we still got nothing, return whatever it was, so that the
+        // database exception will be useful.
+        if (empty($unixTimestamp)) {
+            return $dateString;
+        }
+        return date('Y-m-d H:i:s', $unixTimestamp);
+    }
+}
